eaee17a16b202bd374853492112b8264b9a02a9f
sept-oga <178131979+sept-oga@users.noreply.github.com>
2024-09-30 10:55:08 +0000

    [Break] 4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
+  devise_for :users
+  
+  resources :post_images, only: [:new, :index, :show]
+  
   get 'homes/about', as: 'about' #名前付きルート
 end
